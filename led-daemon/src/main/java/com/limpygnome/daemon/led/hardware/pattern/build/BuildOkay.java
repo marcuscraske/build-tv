090a96aff133030ebde38e88cba7fdc7d82909ab
limpygnome <limpygnome@gmail.com>
2015-10-15 18:19:57 +0100

    LED pattern for build-success / build okay has been changed so that it updates at a frequency. This fixes an issue where the LED strip may be powered by an external resource and replugged in. When this occurs, the LED strip would not update until the actual pattern was updated.

--- led-daemon/src/main/java/com/limpygnome/daemon/led/hardware/pattern/build/BuildOkay.java
+++ led-daemon/src/main/java/com/limpygnome/daemon/led/hardware/pattern/build/BuildOkay.java
@@ -9,11 +9,23 @@ import com.limpygnome.daemon.led.hardware.LedRenderThread;
  */
 public class BuildOkay implements Pattern
 {
+    /**
+     * The pattern is updated at this frequency.
+     *
+     * This is so that if the LEDs are unpowered, they will be updated again at some point in the future.
+     */
+    private static final long IDLE_TIME = 10000;
+
     @Override
     public void render(LedRenderThread ledRenderThread, LedController ledController) throws InterruptedException
     {
-        ledController.setStrip(0, 255, 0);
-        ledController.render();
+        while (!ledRenderThread.isExit())
+        {
+            ledController.setStrip(0, 255, 0);
+            ledController.render();
+
+            Thread.sleep(IDLE_TIME);
+        }
     }
 
     @Override
