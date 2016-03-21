6d31ebf0701944ff29038af730166bd1dbb4971c
limpygnome <limpygnome@gmail.com>
2016-03-21 23:10:59 +0000

    Progress save with share price screen.

--- clients/launcher-client/src/main/resources/website/content/share-price/sharePriceController.js
+++ clients/launcher-client/src/main/resources/website/content/share-price/sharePriceController.js
@@ -13,8 +13,10 @@ $(window).ready(function(){
 */
 sharePriceController = {
 
+    sharePriceSymbol: "ABC",
+
     /* The API to use for querying the share price. - replace ABC.D with stoc */
-    sharePriceApiUrl: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22ABC.D%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=",
+    sharePriceApiUrl: null,
 
     /* The rate at which to poll for changes. */
     pollRate: 30000,
@@ -24,30 +26,55 @@ sharePriceController = {
 
     setup: function()
     {
+        // Build share price URL
+        this.sharePriceApiUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + this.sharePriceSymbol + "%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+
+        // Output config
+        console.debug("sharePriceController - watching symbol: " + this.sharePriceSymbol);
+        console.debug("sharePriceController - usin URL: " + this.sharePriceApiUrl);
+
+        // Update title
+        $("#share-symbol").text(this.sharePriceSymbol);
+
         // Setup polling
         this.setupPoll();
+
+        $("body").snowfall({
+            image: "content/share-price/money.png",
+            minSize: 20,
+            maxSize: 40,
+            round: true
+        });
     },
 
     setupPoll: function()
     {
-        console.info("sharePriceController - polling share price...");
+        setInterval(this.poll, this.pollRate);
+        console.debug("sharePriceController - polling setup - interval: " + this.pollRate);
 
-        setInterval(function(){
-            try
-            {
-                $.getJSON(sharePriceController.sharePriceApiUrl, function(data) {
+        // Poll immediately...
+        this.poll();
+    },
 
-                    sharePriceController.pollHandle(data);
+    poll: function()
+    {
+        try
+        {
+            console.debug("sharePriceController - polling...");
 
-                }).fail(function(){
-                    console.error("sharePriceController - failed to retrieve share price data");
-                });
-            }
-            catch (e)
-            {
-                console.error("sharePriceController - failed to poll: " + e);
-            }
-        }, this.pollRate);
+            $.getJSON(sharePriceController.sharePriceApiUrl, function(data) {
+
+                sharePriceController.pollHandle(data);
+                console.debug("sharePriceController - updated");
+
+            }).fail(function() {
+                console.error("sharePriceController - failed to retrieve share price data");
+            });
+        }
+        catch (e)
+        {
+            console.error("sharePriceController - failed to poll: " + e);
+        }
     },
 
     pollHandle: function(data)
@@ -59,8 +86,81 @@ sharePriceController = {
             return;
         }
 
+        // Update fields
+        var stockData = data["query"]["results"]["quote"];
 
+        this.updateField(stockData, "Open", "#field-open");
+        this.updateTicker(stockData, "Change", "#field-change");
+        this.updateField(stockData, "Ask", "#field-sell");
+        this.updateField(stockData, "Bid", "#field-buy");
+        this.updateField(stockData, "PreviousClose", "#field-previous-close");
+        this.updateTicker(stockData, "ChangeinPercent", "#field-previous-close-change");
 
+        this.updateField(stockData, "YearLow", "#field-year-low");
+        this.updateTicker(stockData, "ChangeFromYearLow", "#field-year-low-change");
+
+        this.updateField(stockData, "YearHigh", "#field-year-high");
+        this.updateTicker(stockData, "ChangeFromYearHigh", "#field-year-high-change");
+
+        this.updateField(stockData, "MarketCapitalization", "#field-market-cap", "£");
+        this.updateField(stockData, "Volume", "#field-volume");
+    },
+
+    updateField: function(stockData, item, target, preSymbol, postSymbol)
+    {
+        var isPreSymbol = (preSymbol != null && preSymbol.length > 0);
+        var isPostSymbol = (postSymbol != null && postSymbol.length > 0);
+
+        var value = stockData[item];
+        var actualValue;
+
+        if (isPreSymbol || isPostSymbol)
+        {
+            // Append symbols...
+            if (preSymbol)
+            {
+                actualValue = value.replace(preSymbol, "");
+
+                if (value.indexOf(preSymbol) == -1)
+                {
+                    value = preSymbol + value;
+                }
+            }
+            else
+            {
+                actualValue = value.replace(postSymbol, "");
+
+                if (value.indexOf(postSymbol) == -1)
+                {
+                    value += postSymbol;
+                }
+            }
+        }
+        else
+        {
+            actualValue = value;
+        }
+
+        // Update value
+        $(target).text(value);
+
+        return actualValue;
+    },
+
+    updateTicker: function(stockData, item, target)
+    {
+        // Update field
+        var value = this.updateField(stockData, item, target, "", "%");
+
+        // Set class based on value being either positive or negative
+        if (value >= 0)
+        {
+            $(target).addClass("ticker-high")
+        }
+        else
+        {
+            $(target).addClass("ticker-low")
+        }
     }
 
 };
