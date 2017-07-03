0e9e86aabdc3359a992d7abcc9be2d1c5e35321f
limpygnome <limpygnome@gmail.com>
2017-07-03 22:46:28 +0100

    X11 now working.

--- deploy/files/setup-scripts/automatic-login.sh
+++ deploy/files/setup-scripts/automatic-login.sh
@@ -7,12 +7,7 @@ if [[ -z "${USER}" ]]; then
     exit 1
 fi
 
-NEW_STR="1:2345:respawn:/bin/login -f ${USER} tty1 </dev/tty1 >/dev/tty1 2>&1"
+# replace lines starting with ExecStart with desired config
+EXEC_START="ExecStart=-/sbin/agetty --noclear --autologin wallboard %I $TERM"
 
-# Remove all lines with tty1 i.e. screen
-sed -i '/tty1/d' /etc/inittab
-
-# Add our line to automatically login desired user
-echo -e "\n\n${NEW_STR}" >> /etc/inittab
-
-echo "Automatic login setup for user: '${USER}'"
+sudo sed -ri "s;ExecStart=.*;${EXEC_START};g" /lib/systemd/system/getty@.service
